bae32232bac249f13d6284dea617c3c7a5a619eb
HuynhNh2k4 <huynhnhu14102004@gmail.com>
2025-03-06 11:23:10 +0700

    lab01

---
+++ lad01/Cau01.py
@@ -0,0 +1,5 @@
+#Nhap ten va tuoi nguoi dung
+ten = input("Nhập tên của bạn: ")
+tuoi = input("Nhập tuổi của bạn: ")
+#In thong diep chao mung voi thong tin vua nhap
+print("Chào mừng,", ten, "! Bạn", tuoi, "tuổi.")
